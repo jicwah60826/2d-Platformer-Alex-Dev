76b0790d0d10eccb62954289c9411e39a0a7f12c
DESKTOP-J3LP9F6\jWhisman <jon@pixelgraft.com>
2024-10-23 15:15:43 -0700

    HandleInput() and HandleMovement() basics setup in Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,38 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D theRB;
+    private Rigidbody2D rb;
+
     [SerializeField]
     private float moveSpeed;
- 
-    // Update is called once per frame
-    void Update()
+
+    private float xInput;
+
+    private void Awake()
     {
- 
-        // Move Player
-        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y); 
- 
+        rb = GetComponent<Rigidbody2D>();
     }
 
+    // Update is called once per frame
+    private void Update()
+    {
+        HandleInput();
+        HandleMovement();
+    }
+
+    private void HandleInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void HandleMovement()
+    {
+        // Move Player
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
 }
