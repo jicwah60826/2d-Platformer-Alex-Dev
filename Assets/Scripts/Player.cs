c1439da73c26e48cef9865b7032525507985c55f
DESKTOP-J3LP9F6\jWhisman <jon@pixelgraft.com>
2024-10-23 18:52:30 -0700

    Player ground check and Jump setup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,12 +8,25 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
 
+    [Header("Movement")]
     [SerializeField]
     private float moveSpeed;
+
+    [SerializeField]
+    private float jumpForce;
 
     private float xInput;
 
     private Animator anim;
+
+    [Header("Collision Info")]
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    [SerializeField]
+    private float groundCheckDistance;
+
+    private bool isGrounded;
 
     private void Awake()
     {
@@ -24,15 +37,38 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        
+        HandleCollision();
         HandleInput();
         HandleMovement();
         HandleAnimations();
     }
 
+    private void HandleCollision()
+    {
+        // Ground Check
+        isGrounded = Physics2D.Raycast(
+            transform.position,
+            Vector2.down,
+            groundCheckDistance,
+            whatIsGround
+        );
+    }
+
     private void HandleInput()
     {
+        // Left / Right input
         xInput = Input.GetAxisRaw("Horizontal");
+
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 
     private void HandleMovement()
@@ -41,8 +77,17 @@ public class Player : MonoBehaviour
         rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
     }
 
-        private void HandleAnimations()
+    private void HandleAnimations()
     {
         anim.SetFloat("xVelocity", rb.velocity.x);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(
+            transform.position,
+            new Vector2(transform.position.x, transform.position.y - groundCheckDistance)
+        );
     }
 }
