245e287ae95461159cac4e7866b5e00191e99940
DESKTOP-J3LP9F6\jWhisman <jon@pixelgraft.com>
2024-10-25 08:25:08 -0700

    2022.3.42f1 commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,8 +14,6 @@ public class Player : MonoBehaviour
     private bool facingRight = true;
     private int facingDir = 1;
 
-    private bool canDoubleJump;
-
     [Header("Movement")]
     [SerializeField]
     private float moveSpeed;
@@ -25,6 +23,16 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     private float doubleJumpForce;
+    private bool canDoubleJump;
+
+    [Header("Wall Interactions")]
+    [SerializeField]
+    private float wallJumpDuration = .6f;
+
+    [SerializeField]
+    private Vector2 wallJumpForce;
+
+    private bool isWallJumping;
 
     [Header("Collision Info")]
     [SerializeField]
@@ -36,8 +44,6 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float wallCheckDistance;
 
-    [SerializeField]
-    private float wallSlideSpeed;
     private bool isGrounded;
     private bool isAirborne;
     private bool isWallDetected;
@@ -58,17 +64,6 @@ public class Player : MonoBehaviour
         HandleFlip();
         HandleCollision();
         HandleAnimations();
-    }
-
-    private void HandleWallSlide()
-    {
-        bool canWallSlide = isWallDetected && rb.velocity.y < 0;
-        float yModifier = yInput < 0 ? 1 : .05f;
-
-        if (canWallSlide == false)
-            return;
-
-        rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * yModifier);
     }
 
     private void UpdateAirborneStatus()
@@ -114,6 +109,10 @@ public class Player : MonoBehaviour
         {
             Jump();
         }
+        else if (isWallDetected && !isGrounded)
+        {
+            WallJump();
+        }
         else if (canDoubleJump)
         {
             DoubleJump();
@@ -124,8 +123,39 @@ public class Player : MonoBehaviour
 
     private void DoubleJump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, doubleJumpForce);
+        isWallJumping = false;
         canDoubleJump = false;
+        rb.velocity = new Vector2(rb.velocity.x, doubleJumpForce);
+    }
+
+    private void WallJump()
+    {
+        canDoubleJump = true;
+        rb.velocity = new Vector2(wallJumpForce.x * -facingDir, wallJumpForce.y);
+
+        Flip();
+        StopCoroutine(WallJumpRoutine());
+        StartCoroutine(WallJumpRoutine());
+    }
+
+    private IEnumerator WallJumpRoutine()
+    {
+        isWallJumping = true;
+
+        yield return new WaitForSeconds(wallJumpDuration);
+
+        isWallJumping = false;
+    }
+
+    private void HandleWallSlide()
+    {
+        bool canWallSlide = isWallDetected && rb.velocity.y < 0;
+        float yModifier = yInput < 0 ? 1 : .05f;
+
+        if (canWallSlide == false)
+            return;
+
+        rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * yModifier);
     }
 
     private void HandleCollision()
@@ -157,9 +187,10 @@ public class Player : MonoBehaviour
     private void HandleMovement()
     {
         if (isWallDetected)
-        {
             return;
-        }
+
+        if (isWallJumping)
+            return;
 
         // Move Player
         rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
@@ -171,15 +202,14 @@ public class Player : MonoBehaviour
         if (xInput < 0 && facingRight || xInput > 0 && !facingRight)
         {
             Flip();
-            facingRight = !facingRight;
         }
     }
 
     private void Flip()
     {
-        // Flip character
         facingDir = facingDir * -1;
         transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
     }
 
     private void OnDrawGizmos()
