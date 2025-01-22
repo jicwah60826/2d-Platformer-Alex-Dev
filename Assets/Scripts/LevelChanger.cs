03dc38dee8083ac0a96e923e5d34bfebdc44a5c0
DESKTOP-J3LP9F6\jWhisman <jon@pixelgraft.com>
2025-01-22 15:29:47 -0800

    Beginning setup of LevelChanger scriptable object setup

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    [SerializeField] private LevelConnection _connection;
+
+}
---
+++ Assets/Scripts/LevelConnection.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenuAttribute(menuName = "Levels/Connection")]
+
+public class LevelConnection : ScriptableObject
+{
+ 
+}
