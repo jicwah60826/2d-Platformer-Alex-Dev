44b0f6392ae6d4c23adb3cbe4710020bef4b3f03
DESKTOP-J3LP9F6\jWhisman <jon@pixelgraft.com>
2025-02-16 18:44:36 -0800

    Cleaning up folders and scripts

--- Assets/Scripts/DamageTrigger.cs
+++ Assets/Scripts/DamageTrigger.cs
@@ -4,10 +4,13 @@ public class DamageTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("DamageTrigger entered");
+
         Player player = other.gameObject.GetComponent<Player>();
 
         if (player != null)
         {
+            Debug.Log("Player Damaged!");
             player.KnockBack(transform.position.x);
         }
     }
